6c12aad09b1a8ec5beeb2932b7dc923799743477
Maintrop4940 <155757346+genius-space4940@users.noreply.github.com>
2024-11-16 18:34:50 +0200

    додати файли

---
+++ script.js
@@ -0,0 +1,15 @@
+let inst = document.querySelector('.inst');
+let tg = document.querySelector('.tg');
+let email = document.querySelector('.email');
+
+inst.addEventListener('click', function() {
+    location.assign('https://www.instagram.com/');
+})
+
+tg.addEventListener('click', function() {
+    location.assign('https://web.telegram.org/');
+})
+
+email.addEventListener('click', function() {
+    location.assign('bonstepan76@gmail.com');
+})
